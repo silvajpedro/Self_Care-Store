01ba7dc1947157bf39c5595f8e9f52332dfb7b92
silvajpedro <belojpedro.31@gmail.com>
2023-05-01 12:03:35 -0300

    update jpg images to svg and map of main images

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,14 +1,6 @@
 import "./Main_style.scss"
 import Card from "./Card/Card"
-import Produto1 from "../../assets/produto4.jpg"
-import Produto2 from "../../assets/produto2.jpg"
-import Produto3 from "../../assets/produto3.jpg"
-import Produto4 from "../../assets/produto5.jpg"
-import Produto5 from "../../assets/produto6.jpg"
-import Produto6 from "../../assets/produto8.jpg"
-import Produto7 from "../../assets/produto7.jpg"
-import Produto9 from "../../assets/produto1.jpg"
-import Produto8 from "../../assets/produto9.jpg"
+import { Produtos } from "./products"
 export default function Main(){
     return(
         <main>
@@ -17,15 +9,9 @@ export default function Main(){
                 <p className="productsText">9 produtos</p>
             </div>
             <section className="products">
-                <Card products={Produto1}/>
-                <Card products={Produto2}/>
-                <Card products={Produto3}/>
-                <Card products={Produto4}/>
-                <Card products={Produto5}/>
-                <Card products={Produto6}/>
-                <Card products={Produto7}/>
-                <Card products={Produto8}/>
-                <Card products={Produto9}/>
+                {Produtos.map((item)=>(
+                    <Card products={item}/>
+                ))}
             </section>
         </main>
     )
---
+++ src/Components/Main/products.jsx
@@ -0,0 +1,21 @@
+import Produto1 from "../../assets/produto4.svg";
+import Produto2 from "../../assets/produto2.svg";
+import Produto3 from "../../assets/produto3.svg";
+import Produto4 from "../../assets/produto5.svg";
+import Produto5 from "../../assets/produto6.svg";
+import Produto6 from "../../assets/produto8.svg";
+import Produto7 from "../../assets/produto7.svg";
+import Produto9 from "../../assets/produto1.svg";
+import Produto8 from "../../assets/produto9.svg";
+
+export const Produtos = [
+  Produto1,
+  Produto2,
+  Produto3,
+  Produto4,
+  Produto5,
+  Produto6,
+  Produto7,
+  Produto8,
+  Produto9
+];
